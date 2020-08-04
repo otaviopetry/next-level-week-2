d9a550fe42b98c4ef90fe665f0d1230ced117991
otaviopetry <otaviopetry@gmail.com>
2020-08-04 01:09:21 -0300

    chore: Initial Commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+### Proffy
+
+Project developed at Next Level Week #2 by <a href="https://www.rocketseat.com.br" alt="Rocketseat">Rocketseat</a>.
+
+Proffy is a platform to connect people who wants to *teach* with people who wants to *learn*.
---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+
+      <button>Click here</button>
+    </div>
+  );
+}
+
+export default App;
