0fcb1a3b165cb7ea749b8ffbf53322a39356e2f5
otaviopetry <otaviopetry@gmail.com>
2020-08-14 01:46:17 -0300

    feat (mobile): PageHeader component

---
+++ mobile/src/components/PageHeader/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 40,
+        paddingTop: 80,
+        backgroundColor: '#8257e5',
+    },
+    topBar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    title: {
+        color: '#fff',
+        fontSize: 24,
+        fontFamily: 'Archivo_700Bold',
+        lineHeight: 32,
+        maxWidth: 180,
+        marginVertical: 40
+    }
+})
+
+export default styles;
