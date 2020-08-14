3d2de250e8c956db1185d415207d0d67c7e2e8f6
otaviopetry <otaviopetry@gmail.com>
2020-08-14 01:22:38 -0300

    feat (mobile): Study bottom tabs styling

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import styles from './styles';
+
 function FavoriteTeachers () {
-    return <View />
+    return <View style={styles.container} />
 }
 
 export default FavoriteTeachers;
